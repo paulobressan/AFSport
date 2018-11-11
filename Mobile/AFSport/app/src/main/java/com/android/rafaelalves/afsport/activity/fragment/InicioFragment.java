d43f3dbb10d5f5b7b39023eca241a8d725317fe3
Rafael Antonio Lopes Alves <32102246+RafaelLopes01@users.noreply.github.com>
2018-11-11 17:40:17 -0200

    Tela de Inicio
    Gráficos da tela inicial

--- Mobile/AFSport/app/src/main/java/com/android/rafaelalves/afsport/activity/fragment/InicioFragment.java
+++ Mobile/AFSport/app/src/main/java/com/android/rafaelalves/afsport/activity/fragment/InicioFragment.java
@@ -9,6 +9,14 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.android.rafaelalves.afsport.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,7 +28,13 @@ public class InicioFragment extends Fragment {
     public InicioFragment() {
         // Required empty public constructor
     }
+    /* Array float, será responsável por definir a porcentagem de cada item do nosso gráfico*/
+    float itensGrafico[] = {98.6f, 56.8f, 35.6f, 45.7f, 10.5f};
+    /* Array string, será responsável por definir o nome  de cada item do nosso gráfico*/
+    String descricao[] = {"1", "2", "3","4","5"};
 
+    /*Grafico*/
+    PieChart grafico;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,7 +42,26 @@ public class InicioFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_inicio, container, false);
 
-        textInicio = view.findViewById(R.id.textInicio);
+        
+
+        grafico = (PieChart) view.findViewById(R.id.graficoID);
+
+        /*Lista criada do tipo <PieChart>*/
+        List<PieEntry> entradasGrafico = new ArrayList<>();
+        /*Preenchendo o grafco*/
+
+        for (int i = 0; i < itensGrafico.length; i++) {
+            entradasGrafico.add(new PieEntry(itensGrafico[i], descricao[i]));
+
+
+        }
+        PieDataSet dataSet = new PieDataSet(entradasGrafico, ("Descrição do Grafico"));
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        PieData pieData = new PieData(dataSet);
+
+        grafico.setData(pieData);
+        grafico.invalidate();
 
         return view;
     }
